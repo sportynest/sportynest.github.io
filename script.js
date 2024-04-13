36baef5fde342dce01dae553dc059f2b20ddc4b3
sportynest <82081186+sportynest@users.noreply.github.com>
2024-04-13 00:11:14 -0400

    added fucntionality and project listing

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ projects.forEach(project => {
         moreInfo.classList.toggle('show-info');
     });
 });
+
+const projectDescriptions = document.querySelectorAll('.project-text');
+
+projectDescriptions.forEach(projectText => {
+    const shortDescription = projectText.querySelector('.short-description');
+    const moreInfo = projectText.querySelector('.more-info-hidden');
+    const readMoreBtn = projectText.querySelector('.read-more');
+
+    readMoreBtn.addEventListener('click', () => {
+        shortDescription.classList.toggle('hidden');
+        moreInfo.classList.toggle('hidden');  
+        readMoreBtn.textContent = readMoreBtn.textContent === 'Read More' ? 'Read Less' : 'Read More';
+    });
+});
